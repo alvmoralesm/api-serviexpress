ae198804c9274e2d3cd74467eb4b9f06cafc55f7
Alfajor <alv.moralesm@duocuc.cl>
2022-06-19 17:25:44 -0400

    Feature: basic settings and middlewares

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ const app = express();
 //settings
 const port = 3000;
 
+//middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//allowing ionic app
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", process.env.IONIC_ROUTE);
+  res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+  if (req.method == "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 //run server
 app.listen(port, () => {
   console.log(`Servidor corriendo en puerto: ${port}`);
